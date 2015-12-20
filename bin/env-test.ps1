68c1f3173129496aa75ef6cd62f3a6e62bc60670
Christian Steinmann <christian.steinmann77@gmail.com>
2015-12-20 03:16:12 +0100

    Add env var script for testing and development

---
+++ bin/env-test.ps1
@@ -0,0 +1,2 @@
+[Environment]::SetEnvironmentVariable('WU_MONGO', 'mongodb://localhost:27017/wanamu-test')
+[Environment]::SetEnvironmentVariable('WU_JWT_SECRET', '12345678')
---
+++ bin/env.cmd
@@ -0,0 +1,2 @@
+SET WU_MONGO=mongodb://localhost:27017/wanamu
+SET WU_JWT_SECRET=12345678
