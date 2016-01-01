b98a5640989ca53f1a7500608caf6f455c2fef29
Christian Steinmann <christian.steinmann77@gmail.com>
2016-01-01 02:07:48 +0100

    Add logging

--- source/v1/middleware/checkTodo.js
+++ source/v1/middleware/checkTodo.js
@@ -1,5 +1,7 @@
 import Todo from '../models/Todo';
 import errors from '../errors';
+import log from '../config/log';
+
 /**
  * Checks if a todo exists and if it belongs to the current user or the user is admin
  * Place this after the auth middleware
@@ -13,6 +15,7 @@ export default async (ctx, next) => {
     const result = {};
 
     if (!todoDoc) {
+        log.error(`[404] Todo with id ${ctx.params.id} not found`);
         ctx.status = 404;
         result.error = new errors.NotFoundError('Todo not found');
         ctx.body = result;
@@ -20,6 +23,7 @@ export default async (ctx, next) => {
     }
 
     if (!todoDoc.owner.equals(ctx.user._id) && !ctx.user.isAdmin) {
+        log.error(`[403] ${ctx.user.username} does not have enough right to get Todo with id ${ctx.params.id}`);
         ctx.status = 403;
         result.error = new errors.AccessDeniedError('Not enough rights to share this todo');
         ctx.body = result;
