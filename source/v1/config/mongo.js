64f6491ca4d88375c55d597e050a122349ba6235
Christian Steinmann <christian.steinmann77@gmail.com>
2016-03-20 23:27:00 +0100

    Refactor drop database to work with mongolab

--- source/v1/config/mongo.js
+++ source/v1/config/mongo.js
@@ -14,7 +14,17 @@ const mongoUtil = {
         });
     },
     drop() {
-        return mongoose.connection.db.dropDatabase();
+        return new BluePromise((resolve, reject) => {
+            mongoose.connection.db.collections().then(function (colls) {
+                const promises = [];
+                colls.forEach((e) => {
+                    if(e.s.name.indexOf('System.') === -1 && e.s.name.indexOf('system.') === -1) {
+                        promises.push(mongoose.connection.db.dropCollection(e.s.name));
+                    }
+                });
+                BluePromise.all(promises).then(resolve).catch(reject);
+            });
+        });
     },
     dropCollection(coll) {
         return mongoose.connection.db.dropCollection(coll);
