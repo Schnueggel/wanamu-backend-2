db1a628f59c72261c83f3e3ec72a082518fdd82c
Christian Steinmann <christian.steinmann77@gmail.com>
2016-03-26 17:57:39 +0100

    Update docker files

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ ADD ./dist /wanamu-backend-2/dist
 ADD ./package.json /wanamu-backend-2
 WORKDIR /wanamu-backend-2
 RUN npm install --production
-ENV WU_REDIS_HOST=$REDIS_PORT_6379_TCP_ADDR
-ENV WU_MONGO_HOST=$MONGO_PORT_27017_TCP_ADDR
-CMD npm run start && npm run pm-log
+EXPOSE 3000
+CMD ["npm run start && npm run pm-log"]
--- bin/env-test.ps1
+++
@@ -1,7 +0,0 @@
-[Environment]::SetEnvironmentVariable('WU_MONGO', 'mongodb://localhost:27017/wanamu-test')
-[Environment]::SetEnvironmentVariable('WU_JWT_SECRET', '12345678')
-[Environment]::SetEnvironmentVariable('WU_SOCKET_PORT', '8888')
-[Environment]::SetEnvironmentVariable('WU_REDIS_HOST', 'localhost')
-[Environment]::SetEnvironmentVariable('PORT', '9999')
-[Environment]::SetEnvironmentVariable('WU_CPUS', '4')
-[Environment]::SetEnvironmentVariable('WU_APP_NAME', 'Wuhuhu')
